cc3167093e93165e0a8d7786e34b9da19a8e5f50
Adam Wieckert <adam.wieckert@ncontracts.com>
2021-12-10 08:22:55 -0600

    first commit

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace adventOfCode_awieckert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
